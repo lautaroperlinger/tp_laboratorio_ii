536f8d169a4a229d7a4577c981ed6f15e06751d1
lautaroperlinger <lautarothomasperlinger@hotmail.com>
2022-04-17 21:08:22 -0300

    1

---
+++ TP_1/TP_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
